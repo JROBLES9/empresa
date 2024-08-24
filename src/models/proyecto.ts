0b3eea32718d5e46d10972b298575518eac6af52
Josué Robles <88120613+JROBLES9@users.noreply.github.com>
2024-08-24 11:24:05 -0600

    chore: Refactor ProyectoModel to use Sequelize Model class

--- src/models/proyecto.ts
+++ src/models/proyecto.ts
@@ -1,33 +1,51 @@
-import { DataTypes } from 'sequelize';
+import { Model, DataTypes } from 'sequelize';
 import sequelize from '../db/db';
+import { Proyecto } from '../@types/globals';
 
-const proyectoModel = sequelize.define('proyecto', {
+class ProyectoModel extends Model<Proyecto> implements Proyecto {
+    public id_proyecto!: number;
+    public nombre_proyecto!: string;
+    public descripcion!: string;
+    public fecha_inicio!: Date;
+    public fecha_fin!: Date;
+    public porcentaje_completado!: number;
+    public estado!: string;
+}
+
+ProyectoModel.init({
     id_proyecto: {
         type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+        autoIncrement: true,
+        primaryKey: true
     },
     nombre_proyecto: {
-        type: DataTypes.STRING(100)
+        type: DataTypes.STRING,
+        allowNull: false
     },
     descripcion: {
-        type: DataTypes.TEXT
+        type: DataTypes.STRING,
+        allowNull: false
     },
     fecha_inicio: {
-        type: DataTypes.DATE
+        type: DataTypes.DATE,
+        allowNull: false
     },
     fecha_fin: {
-        type: DataTypes.DATE
+        type: DataTypes.DATE,
+        allowNull: false
     },
     porcentaje_completado: {
-        type: DataTypes.DECIMAL(5, 2)
+        type: DataTypes.INTEGER,
+        allowNull: false
     },
     estado: {
-        type: DataTypes.STRING(20)
+        type: DataTypes.STRING,
+        allowNull: false
     }
 }, {
-    tableName: 'proyectos',
+    sequelize,
+    modelName: 'proyectos',
     timestamps: false
 });
 
-export default proyectoModel;
+export default ProyectoModel;
